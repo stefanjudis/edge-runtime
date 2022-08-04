664092f402f814ffab05ac881832e12a70138cd3
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-08-04 13:39:23 +0200

    test(primitives): add abort-controller cases

--- packages/primitives/tests/abort-controller.test.ts
+++ packages/primitives/tests/abort-controller.test.ts
@@ -14,3 +14,35 @@ it('allows to abort fetch', async () => {
     expect(error.message).toEqual('The operation was aborted')
   }
 })
+
+it('AbortSignal.abort is defined', () => {
+  expect(typeof AbortSignal.abort).toBe('function')
+})
+
+it('AbortSignal.timeout is defined', () => {
+  expect(typeof AbortSignal.timeout).toBe('function')
+})
+
+it('AbortSignal.reason is present', () => {
+  const controller = new AbortController()
+  controller.abort()
+  const signal = controller.signal
+
+  expect(!!signal.reason).toBe(true)
+})
+
+it('AbortSignal.aborted is present', () => {
+  const controller = new AbortController()
+  controller.abort()
+  const signal = controller.signal
+
+  expect(!!signal.aborted).toBe(true)
+})
+
+it('AbortSignal.throwIfAborted is present', () => {
+  const controller = new AbortController()
+  controller.abort()
+  const signal = controller.signal
+
+  expect(!!signal.throwIfAborted).toBe(true)
+})
